45662fb4a5d30d2620e60584cd8969da3fbc48f3
Ramunas <rambek@gmail.com>
2019-10-02 14:24:53 +0300

    Gallery HTML render in js

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,6 @@
 renderMy_Services( my_services );
 renderBlogsImages( My_blogs );
 window.onscroll = function() {myFunction()};
+
+// pirmas parode kur, antras kuriuos duomenis naudosime
+renderPortfolio ( '#our-work > .row:nth-child(2)', portfolio );
--- js/functions.js
+++ js/functions.js
@@ -57,3 +57,98 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+function renderPortfolio ( target, data ) {
+  let filter_HTML = `<div class="filter-items"></div>
+                      <div class="filter-items"></div>
+                      <div class="filter-items"></div>`;
+
+let galery_HTML = ``
+  for ( let i=0; i<data.length; i++ ){
+    galery_HTML += `<div class="portfolio_images">
+                      <div class="Portf_img">
+                        <img class="Portf_img" src="./img/portfolio-images/${data[i].pic}"></img>
+                        <div class="images-block">
+                          <div class="images-txt">Our photography</div>
+                        </div>
+                      </div>
+                    </div>`;
+  }
+
+  let HTML = `<div class="filter">
+                <div class="filter-items">All</div>
+                ${filter_HTML}
+              </div>
+              ${galery_HTML}`;
+  
+  return document.querySelector(target).innerHTML = HTML;
+}
+
+
+
+
+// function renderPortfolio( target, data ) {
+//   let filter_HTML = '';
+//   let unique_tags = [];
+
+//   for ( let i=0; i<data.length; i++ ) {
+//     let category = data[i].cat.toLowerCase();
+//     if ( unique_tags.indexOf(category) === -1 ){
+//       unique_tags.push(category);
+//     }
+//   }
+
+//   for ( let i=0; i<unique_tags.length; i++ ) {
+//     filter_HTML += `<div class ="portfolio_categories">${unique_tags[i]}</div>`;
+//   }
+
+//   for ( let i=0; i<data.length; i++ ) {
+//     galery_HTML += `<div class="portfolio_categories" data-category="${data[i].cat.toLowerCase()}">
+//     <div class="portfolio_images">
+//         <img src="./img/portfolio-images/${data[i].pic}"></img>
+//         <div class="images-block">
+//             <div class="images-txt">Our photography</div>
+//         </div>
+//     </div>`
+//   }
+//   let HTML = `<div class="portfolio">
+//   //                   <div class="filter">
+//   //                       <div class="filter-item active">All works</div>
+//   //                       ${filter_HTML}
+//   //                   </div>
+//   //                   <div class="item-list">
+//   //                       ${gallery_HTML}
+//   //                   </div>
+//   //               </div>`;
+
+
+//   return document.querySelector(target).innerHTML = HTML;
+// }
+
+
+
+
+//   let gallery_HTML =  '';
+//   for ( let i=0; i<data.length; i++ ) {
+//       gallery_HTML += `<div class="item" data-category="${data[i].cat.toLowerCase()}">
+//                           <div>IMAGE: ${data[i].pic}</div>
+//                           <div>TITLE: ${data[i].title}</div>
+//                       </div>`;
+//   }
+
+//   // <div>CATEGORY: <span class="cat">${data[i].cat.toLowerCase()}</span></div>
+
+//   // render complete HTML
+//   let HTML = `<div class="gallery">
+//                   <div class="filter">
+//                       <div class="filter-item active">All works</div>
+//                       ${filter_HTML}
+//                   </div>
+//                   <div class="item-list">
+//                       ${gallery_HTML}
+//                   </div>
+//               </div>`;
+  
+//   // include complete HTML into targeted element
+//   return document.querySelector(target).innerHTML = HTML;
+// }
